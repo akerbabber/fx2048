61bc3f7e2363f71663d71c33b7cb3747c4c261e1
Davine <setzu.mattia@gmail.com>
2014-07-11 19:06:53 +0200

    Deleted MyGiocatoreAutomatico.java by mistake

---
+++ src/giocatoreAutomatico/player/MyGiocatoreAutomatico.java
@@ -0,0 +1,129 @@
+package giocatoreAutomatico.player;
+
+import game2048.*;
+import giocatoreAutomatico.*;
+import java.util.Set;
+
+public class MyGiocatoreAutomatico implements GiocatoreAutomatico {
+    
+    final int MONOTONYWEIGHT=5;
+    final int CONTIGUITYWEIGHT=1;
+    final int FREECELLSWIEGHT=10;
+    final int MERGEWEIGHT=5;
+    final int UPPERSIDEWEIGHT=10;
+    final int RIGHTSIDEWEIGHT=9;
+    final int LEFTSIDEWEIGHT=9;
+    final int DOWNSIDEWEIGHT=4;
+    final int PREVISIONTREEDEPTH=2;
+
+    /**
+     * Select next move.
+     * 
+     * @param  gameG The game grid.
+     * 
+     * @return   0 == Direction.UP | 1 == Direction.RIGHT | 2 == Direction.DOWN | 3 == Direction.LEFT
+     */
+    public int prossimaMossa(Griglia gameGrid)
+    {
+      
+        return this.arbiter(new MatrixGriglia(gameGrid));
+       
+    }  
+
+    /**
+     * Calculate move poridints.
+     * 
+     * @param  grid       The game grid.
+     * @param  dir        The move direction.
+     * @param  ricorsioni Current tree level.
+     * 
+     * @return            The move points.
+     */
+    public int pointsMove(MatrixGriglia grid, Direction dir, int ricorsioni, int tot) 
+    {
+
+        MatrixGriglia auxGrid=grid.copy();
+        int punti;
+
+        /* Emulate move */
+        auxGrid=auxGrid.emulateMove(dir);
+        
+        
+        if(ricorsioni>0)
+        {
+            ricorsioni--;
+
+            tot+= pointsMove(auxGrid.emulateMove(Direction.UP), Direction.UP, ricorsioni, tot);
+            tot+= pointsMove(auxGrid.emulateMove(Direction.RIGHT),Direction.RIGHT, ricorsioni, tot);
+            tot+= pointsMove(auxGrid.emulateMove(Direction.DOWN), Direction.DOWN, ricorsioni, tot);
+            tot+= pointsMove(auxGrid.emulateMove(Direction.LEFT), Direction.LEFT, ricorsioni, tot);
+
+            return tot; 
+        
+        }   
+        
+        punti=grid.puntiMerge(dir)+auxGrid.contiguity()*CONTIGUITYWEIGHT+auxGrid.freeCells()*FREECELLSWIEGHT+auxGrid.monotony()*MONOTONYWEIGHT;
+        
+        return punti;
+        
+    }
+
+    /**
+     * Choose the best move out of the 4 available.
+     * 
+     * @param  oldGriglia The game grid.
+     * 
+     * @return            The best move. At least better than the one we'd do.
+     */
+    public int arbiter(MatrixGriglia oldGriglia) {
+        
+        int punti = 0;
+        int dir = 1;
+    
+        /* Right move points*/
+        if (!(oldGriglia.equals(oldGriglia.emulateMove(Direction.RIGHT)))) {
+            
+            punti = RIGHTSIDEWEIGHT*(pointsMove(oldGriglia,Direction.RIGHT, PREVISIONTREEDEPTH, 0));
+            
+        }
+
+        /* UP POINTS */
+        if (!(oldGriglia.equals(oldGriglia.emulateMove(Direction.UP)))) {
+
+            if (punti < UPPERSIDEWEIGHT*(pointsMove(oldGriglia,Direction.UP, PREVISIONTREEDEPTH, 0))) {
+
+                punti = UPPERSIDEWEIGHT*(pointsMove(oldGriglia,Direction.UP, PREVISIONTREEDEPTH, 0));
+                dir = 0;              
+
+            }
+
+        }
+
+        /* LEFT POINTS */
+        if (!(oldGriglia.equals(oldGriglia.emulateMove(Direction.LEFT)))) {
+
+            if (punti < LEFTSIDEWEIGHT*(pointsMove(oldGriglia,Direction.LEFT, PREVISIONTREEDEPTH, 0))) {
+
+                punti = LEFTSIDEWEIGHT*(pointsMove(oldGriglia,Direction.LEFT, PREVISIONTREEDEPTH, 0));
+                dir = 3;
+
+            }
+
+        }
+
+        /* DOWN POINTS */
+        if (!(oldGriglia.equals(oldGriglia.emulateMove(Direction.DOWN)))) {
+
+            if (punti < DOWNSIDEWEIGHT*(pointsMove(oldGriglia,Direction.DOWN, PREVISIONTREEDEPTH, 0))) {
+
+                dir = 2;
+
+            }
+
+        }
+
+        return dir;
+
+    }
+
+}
